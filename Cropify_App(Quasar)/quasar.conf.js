2b581cdc74044197e60d3ebc23be7ec6c4959b50
Mayank Shinde <30957986+mayank31313@users.noreply.github.com>
2020-08-01 15:55:56 +0530

    kuzzle-edited

--- Cropify_App(Quasar)/quasar.conf.js
+++ Cropify_App(Quasar)/quasar.conf.js
@@ -17,12 +17,12 @@ module.exports = function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
-    boot: [
-      
+    boot: [     
       'axios',
       'firebase',
       'vuechartkick',
-      'vue-google-charts'
+      'vue-google-charts',
+      "kuzzle"
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
---
+++ Cropify_App(Quasar)/src/boot/kuzzle.js
@@ -0,0 +1,2 @@
+import { Kuzzle, WebSocket } from 'kuzzle-sdk';
+export default new Kuzzle(new WebSocket('localhost'));
